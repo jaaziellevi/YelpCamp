296c9b4ddf98e91408e541b801b580edb318d339
Jaaziel <jaaziel.levi@gmail.com>
2018-03-29 21:41:53 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-//mongoose.connect(process.env.DATABASEURL);
-mongoose.connect("mongodb://jaaziel:maria@ds139262.mlab.com:39262/yelpcampstudy");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://jaaziel:maria@ds139262.mlab.com:39262/yelpcampstudy");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
